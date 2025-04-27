dc8958304c973f78ec9a3ee18ec5501c5321ebe9
unknown <joao.barbosa@canopusonline.com>
2025-04-27 10:31:02 -0300

    AJustes teste de rota liberada

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,31 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express();
-const positionRoutes = require('./positionRoutes');
-const movimentRoutes = require('./movimentRoutes');
-const categoryRoutes = require('./categoryRoutes');
-const userRoutes = require('./userRoutes');
-const consultRoutes = require('./consultRoutes');
-const requestsRoutes = require('./requestsRoutes');
-const withdrawRoutes = require('./withdrawRoutes');
-const assessmentsRoutes = require('./assessmentsRoutes');
-const productsRoutes = require('./productsRoutes');
-const mercadoPagoRoutes = require('./mercadoPagoRoutes');
-const carrosselRoutes = require('./carrosselRoutes');
-const addressRoutes = require('./addressRoutes');
-const productsController = require('../controllers/productsController');
-const categoryController = require('../controllers/categoryController');
-const carrosselController = require('../controllers/carrosselController');
-const addressController = require('../controllers/addressController');
-const requestsController = require('../controllers/requestsController');
+const positionRoutes = require("./positionRoutes");
+const movimentRoutes = require("./movimentRoutes");
+const categoryRoutes = require("./categoryRoutes");
+const userRoutes = require("./userRoutes");
+const consultRoutes = require("./consultRoutes");
+const requestsRoutes = require("./requestsRoutes");
+const withdrawRoutes = require("./withdrawRoutes");
+const assessmentsRoutes = require("./assessmentsRoutes");
+const productsRoutes = require("./productsRoutes");
+const mercadoPagoRoutes = require("./mercadoPagoRoutes");
+const carrosselRoutes = require("./carrosselRoutes");
+const addressRoutes = require("./addressRoutes");
+const productsController = require("../controllers/productsController");
+const categoryController = require("../controllers/categoryController");
+const carrosselController = require("../controllers/carrosselController");
+const addressController = require("../controllers/addressController");
+const requestsController = require("../controllers/requestsController");
 
 router.use(positionRoutes);
 router.use(mercadoPagoRoutes);
-router.get('/produtos/:id', productsController.listProducts);
-router.get('/maisvendidos', productsController.getTop5ProdutosMaisVendidos)
-router.get('/categorias', categoryController.listCategorys);
-router.get('/carrossel/:id', carrosselController.listCarrossel);
-router.post('/endereco', addressController.addAddress);
-router.post('/pedidos/web', requestsController.addRequestUnlogged)
+router.get("/produtos/:id", productsController.listProducts);
+router.get("/maisvendidos", productsController.getTop5ProdutosMaisVendidos);
+router.get("/categorias", categoryController.listCategorys);
+router.get("/carrossel/:id", carrosselController.listCarrossel);
+router.post("/endereco", addressController.addAddress);
+router.post("/pedidos/web", requestsController.addRequestUnlogged);
+router.post(
+  "/pedidos/produtos/:id",
+  requestsController.editRequestWithProducts
+);
+
 router.use(userRoutes);
 router.use(categoryRoutes);
 router.use(consultRoutes);
