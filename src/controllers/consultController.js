6674028c70862d3b8616222c6ce2a32cc280ab5f
unknown <joao.barbosa@canopusonline.com>
2025-05-15 14:45:13 -0300

    Exibindo log caso o email esteja dando erro ao invez de retornar erro 500

--- src/controllers/consultController.js
+++ src/controllers/consultController.js
@@ -158,39 +158,47 @@ const bloqConsult = async (req, res) => {
       .returning('*');
 
     if(opt === 1) {
-      mailer.sendMail(
-        {
-          to: email,
-          bcc: process.env.BIODERMIS_MAIL,
-          from: process.env.FROM_MAIL,
-          template: './userActive',
-          subject: `✅ Cadastro Aprovado! Você já pode começar a revender Biodermis!`,
-          context: {
-            nome,
+      try {
+        mailer.sendMail(
+          {
+            to: email,
+            bcc: process.env.BIODERMIS_MAIL,
+            from: process.env.FROM_MAIL,
+            template: './userActive',
+            subject: `✅ Cadastro Aprovado! Você já pode começar a revender Biodermis!`,
+            context: {
+              nome,
+            },
           },
-        },
-        (err) => {
-          if (err)
-            console.log(err)
-        },
-      )
+          (err) => {
+            if (err)
+              console.log(err)
+          },
+        )
+      } catch (error) {
+        console.log('Erro ao tentar enviar e-mail:', err);
+      }
     } else {
-      mailer.sendMail(
-        {
-          to: email,
-          bcc: process.env.BIODERMIS_MAIL,
-          from: process.env.FROM_MAIL,
-          template: './userBloq',
-          subject: `Infelizmente, seu cadastro não pôde ser aprovado.`,
-          context: {
-            nome,
+      try {
+        mailer.sendMail(
+          {
+            to: email,
+            bcc: process.env.BIODERMIS_MAIL,
+            from: process.env.FROM_MAIL,
+            template: './userBloq',
+            subject: `Infelizmente, seu cadastro não pôde ser aprovado.`,
+            context: {
+              nome,
+            },
           },
-        },
-        (err) => {
-          if (err)
-            console.log(err)
-        },
-      )
+          (err) => {
+            if (err)
+              console.log(err)
+          },
+        )
+      } catch (error) {
+        console.log('Erro ao tentar enviar e-mail:', err);
+      }
     }
 
     return res.status(200).json({ success: 'Status alterado com sucesso!' });
